f429bfdddbed0cffd85be859f16f98894a844638
Petar Yankov <petarroma@gmail.com>
2022-01-05 08:41:46 +0200

    Add user input validation.

--- ProjectMortgageCalculator/src/com/training/Main.java
+++ ProjectMortgageCalculator/src/com/training/Main.java
@@ -9,18 +9,40 @@ public class Main {
         final byte NUMBER_OF_MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
 
+        int principal = 0;
+        float monthlyInterest = 0;
+        int numberOfPayments = 0;
+
+
         var scanner = new Scanner(System.in);
 
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        while (true) {
+            System.out.print("Principal ($10,000 - $1,000,000): ");
+            principal = scanner.nextInt();
+            if (principal >= 10_000 && principal <= 1_000_000)
+                break;
+            System.out.println("Enter a principal amount between $10,000 and $1,000,000.");
+        }
 
-        System.out.print("Annual interest: ");
-        float annualInterest = scanner.nextFloat();
-        float monthlyInterest = annualInterest / PERCENT / NUMBER_OF_MONTHS_IN_YEAR;
+        while (true) {
+            System.out.print("Annual interest (1% - 20%): ");
+            float annualInterest = scanner.nextFloat();
+            if (annualInterest >= 1 && annualInterest <= 20) {
+                monthlyInterest = annualInterest / PERCENT / NUMBER_OF_MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter an annual interest value between 1% and 20%.");
+        }
 
-        System.out.print("Period: ");
-        int period = scanner.nextInt();
-        int numberOfPayments = period * NUMBER_OF_MONTHS_IN_YEAR;
+        while (true) {
+           System.out.print("Period (5 - 50 years): ");
+           int period = scanner.nextInt();
+           if (period >= 5 && period <= 50) {
+               numberOfPayments = period * NUMBER_OF_MONTHS_IN_YEAR;
+               break;
+           }
+           System.out.println("Enter a period value between 5 and 50 years.");
+       }
 
         double mortgage = principal
                 * ((monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
